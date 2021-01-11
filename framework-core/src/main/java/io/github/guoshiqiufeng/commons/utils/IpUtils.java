bb40c609013f7d415ee8e22e827b34dc1814a1e5
yanghq <1040926235@qq.com>
2021-01-11 09:47:27 +0800

    优化

--- framework-core/src/main/java/io/github/guoshiqiufeng/commons/utils/IpUtils.java
+++ framework-core/src/main/java/io/github/guoshiqiufeng/commons/utils/IpUtils.java
@@ -25,11 +25,14 @@ public class IpUtils {
 	private static final String HTTP_X_FORWARDED_HEADER = "HTTP_X_FORWARDED_FOR";
 
 	private static final String ERROR_MSG = "IPUtils ERROR ";
+
 	/**
 	 * 获取IP地址
 	 *
 	 * 使用Nginx等反向代理软件， 则不能通过request.getRemoteAddr()获取IP地址
 	 * 如果使用了多级反向代理的话，X-Forwarded-For的值并不止一个，而是一串IP地址，X-Forwarded-For中第一个非unknown的有效IP字符串，则为真实IP地址
+	 * @param request 请求
+	 * @return ip
 	 */
 	public static String getIpAddress(HttpServletRequest request) {
 		String ip = null;
